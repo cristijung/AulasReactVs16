22805eb34cc1d49e25a285e0d1eb8e68b9a740e7
CristinaJung <cristijung@gmail.com>
2025-11-17 10:12:43 -0300

    ajustes

--- ServerClient/client-server/types/ImageProps.ts
+++ ServerClient/client-server/types/ImageProps.ts
@@ -1,13 +1,13 @@
 
 import { ImageProps } from 'next/image';
 
-export interface ResponsiveImageProps
+export interface ImageProps
   // TypeScript usa o Omit (um Utility Type) para criar uma nova interface (ResponsiveImageProps) 
   // a partir da interface padrão do Next.js (ImageProps), removendo quatro propriedades: alt, layout, fill e style.
   // Omit<...>: Diz ao TypeScript para herdar todas as propriedades de ImageProps exceto aquelas listadas.
   extends Omit<ImageProps, 'alt' | 'layout' | 'fill' | 'style'> {
   
-    //  texto alternativo para acessibilidade. **OBRIGATÓRIO** para WCAG.   
+  //  texto alternativo para acessibilidade. **OBRIGATÓRIO** para WCAG.   
   alt: string;
   
   // classes tailwind para estilização do contêiner (ex: width, margin, border-radius) - opcional.   
